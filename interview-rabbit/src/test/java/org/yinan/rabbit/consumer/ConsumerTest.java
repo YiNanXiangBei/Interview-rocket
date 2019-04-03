119399a0e8b452d16202596759215994743abf59
kanon <vipkia@sina.cn>
2019-04-03 15:45:40 +0800

    修改consumer的死循环为condition

--- interview-rabbit/src/test/java/org/yinan/rabbit/consumer/ConsumerTest.java
+++ interview-rabbit/src/test/java/org/yinan/rabbit/consumer/ConsumerTest.java
@@ -3,6 +3,7 @@ package org.yinan.rabbit.consumer;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,13 +15,22 @@ import static org.junit.Assert.*;
  */
 public class ConsumerTest {
 
-    Consumer consumer = new Consumer();
+    private Consumer consumer = new Consumer();
+
+    private static final Lock LOCK = new ReentrantLock();
+
+    private static final Condition STOP = LOCK.newCondition();
 
     @Test
     public void receiver() throws IOException {
         consumer.receiver();
-        for (;;) {
-
+        try {
+            LOCK.lock();
+            STOP.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            LOCK.unlock();
         }
 
     }

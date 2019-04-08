49a56cf7d001ce33e31fd5249864981a0f9267a4
kanon <vipkia@sina.cn>
2019-04-08 17:27:43 +0800

    添加spring事务

---
+++ interview-transaction/src/main/java/org/yinan/spring/config/DataSourceConfig.java
@@ -0,0 +1,9 @@
+package org.yinan.spring.config;
+
+/**
+ * @author yinan
+ * @date created in 下午4:15 19-4-8
+ */
+public class DataSourceConfig {
+
+}
---
+++ interview-transaction/src/main/java/org/yinan/spring/service/AmountService.java
@@ -0,0 +1,9 @@
+package org.yinan.spring.service;
+
+/**
+ * @author yinan
+ * @date created in 下午4:19 19-4-8
+ */
+public class AmountService {
+
+}

6a344b3240e17e72154e0dabbd3987f642b2c863
kanon <vipkia@sina.cn>
2019-04-19 16:16:45 +0800

    添加基准测试案例

---
+++ interview-jmh/src/main/java/org/yinan/jmh/StringBuilderBenchmark.java
@@ -0,0 +1,52 @@
+package org.yinan.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yinan
+ * @date created in 下午3:00 19-4-19
+ */
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 3)
+@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS)
+@Threads(8)
+@Fork(2)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class StringBuilderBenchmark {
+
+    private void print(String a) {
+        System.out.println(a);
+    }
+
+    @Benchmark
+    public void testStringAdd() {
+        String a = "";
+        for (int i = 0; i < 10; i++) {
+            a += i;
+        }
+        print(a);
+    }
+
+    @Benchmark
+    public void testStringBuildAdd() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(i);
+        }
+        print(sb.toString());
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+                .include(StringBuilderBenchmark.class.getSimpleName())
+                .output("/home/laowang/testlog/benchmark.log")
+                .build();
+        new Runner(options).run();
+    }
+}
---
+++ interview-jmh/target/generated-sources/annotations/org/yinan/jmh/generated/StringBuilderBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package org.yinan.jmh.generated;
+public class StringBuilderBenchmark_jmhType extends StringBuilderBenchmark_jmhType_B3 {
+}
+
